f23a1384a5943defe26f162a9c9e8f81f2adf6d1
xyl6716 <xyl6716@outlook.com>
2025-05-25 14:01:03 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.gitignore
    #	.idea/vcs.xml

--- puzzle/main.cpp
+++ puzzle/main.cpp
@@ -1,5 +1,5 @@
 //
-// Created by 27847 on 2025/5/25.
+// Created by ApolloMonasa on 2025/5/25.
 //
 
 #include <bits/stdc++.h>
@@ -7,7 +7,7 @@ using namespace std;
 
 int main() {
   ios::sync_with_stdio(false);
-  cin.tie(0);
-  cout << "hello!";
+  cin.tie(nullptr);
+
   return 0;
 }
